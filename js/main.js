effd1018ded5441c3eb8a4bc97ba1d82f489225b
srsexton94 <srsexton94@gmail.com>
2025-06-10 22:34:22 -0400

    add randomization to avatars

--- js/main.js
+++ js/main.js
@@ -1,10 +1,56 @@
+const SKIN_COLORS = [
+  { hex: "694d3d", label: "dark" },
+  { hex: "ae5d29", label: "medium" },
+  { hex: "d08b5b", label: "tan" },
+  { hex: "edb98a", label: "light" },
+  { hex: "ffdbb4", label: "pale" },
+];
+const HEAD_STYLES = [
+  { code: "longAfro", description: "an afro", hasHeadCovering: false },
+  { code: "bangs", description: "bangs", hasHeadCovering: false },
+  { code: "bantuKnots", description: "bantu knots", hasHeadCovering: false },
+  { code: "bun", description: "a bun with a headband", hasHeadCovering: true },
+  { code: "cornrows2", description: "cornrows", hasHeadCovering: false },
+  { code: "dreads", description: "locs", hasHeadCovering: false },
+  { code: "flatTopLong", description: "a flat top", hasHeadCovering: false },
+  { code: "grayBun", description: "a bun", hasHeadCovering: false },
+  { code: "hatBeanie", description: "a beanie", hasHeadCovering: true },
+  { code: "hatHip", description: "a wide-brimmed hat", hasHeadCovering: true },
+  { code: "hijab", description: "a hijab", hasHeadCovering: true },
+  { code: "long", description: "long hair", hasHeadCovering: false },
+  { code: "longCurly", description: "curly hair", hasHeadCovering: false },
+  { code: "mohawk", description: "a mohawk", hasHeadCovering: false },
+  { code: "noHair1", description: "no hair", hasHeadCovering: false },
+  { code: "shaved2", description: "a high fade", hasHeadCovering: false },
+  { code: "short4", description: "short hair", hasHeadCovering: false },
+  { code: "turban", description: "a turban", hasHeadCovering: true },
+];
+const NAMES = [
+  "Bailey",
+  "Akira",
+  "Cameron",
+  "Ming",
+  "Taylor",
+  "Alex",
+  "Sasha",
+  "Jesse",
+  "Riley",
+  "Sam",
+  "Morgan",
+  "Quinn",
+  "Anh",
+  "Sol",
+  "Meko",
+  "Skylar",
+];
+
 const gameboard = document.querySelector("section.gameboard");
-let boardArr = Array.from({ length: 16 });
+let boardArr;
 
 function createCardHTML(src, alt) {
   return `
     <div class="card">
-      <div class="inner flipped" onclick="onCardClick(this)">
+      <div class="inner flipped">
         <div class="front"></div>
         <img
           class="back"
@@ -22,15 +68,30 @@ function createElementFromHTML(htmlString) {
   return container.firstElementChild;
 }
 
-function onCardClick(card) {
-  card.classList.toggle("flipped");
+function getRandomHex() {
+  return Math.floor(Math.random() * 16777215).toString(16);
+}
+
+function getRandomArrayEl(array) {
+  const randomIndex = Math.floor(Math.random() * array.length);
+  return array[randomIndex];
 }
 
 window.addEventListener("load", function () {
-  boardArr = boardArr.map((_el) => {
+  boardArr = NAMES.map((name) => {
+    const isFacingLeft = Math.random() >= 0.5;
+    const skinColor = getRandomArrayEl(SKIN_COLORS);
+    const headStyle = getRandomArrayEl(HEAD_STYLES);
     return {
-      src: "https://api.dicebear.com/9.x/open-peeps/svg",
-      alt: "avatar",
+      src: `https://api.dicebear.com/9.x/open-peeps/svg?face=calm&backgroundColor=${getRandomHex()}&flip=${isFacingLeft}&skinColor=${
+        skinColor.hex
+      }&head=${headStyle.code}`,
+      alt: `An avatar with ${skinColor.label} skin and ${
+        headStyle.description
+      } facing ${isFacingLeft ? "left" : "right"}`,
+      name,
+      skinColor,
+      headStyle,
     };
   });
   boardArr.forEach((el) => {
@@ -38,4 +99,8 @@ window.addEventListener("load", function () {
       createElementFromHTML(createCardHTML(el.src, el.alt))
     );
   });
+});
+
+gameboard.addEventListener("click", function (event) {
+  event.target.parentNode.classList.toggle("flipped");
 });
