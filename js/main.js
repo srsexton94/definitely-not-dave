5d3e1e5c1ce46d25f0407e8e9dbafb2ddf3dfe89
srsexton94 <srsexton94@gmail.com>
2025-06-10 21:19:10 -0400

    create a board of flippable cards

--- js/main.js
+++ js/main.js
@@ -1,1 +1,41 @@
-console.log("JS loaded");
+const gameboard = document.querySelector("section.gameboard");
+let boardArr = Array.from({ length: 16 });
+
+function createCardHTML(src, alt) {
+  return `
+    <div class="card">
+      <div class="inner flipped" onclick="onCardClick(this)">
+        <div class="front"></div>
+        <img
+          class="back"
+          src="${src}"
+          alt="${alt}"
+        />
+      </div>
+    </div>
+  `;
+}
+
+function createElementFromHTML(htmlString) {
+  const container = document.createElement("div");
+  container.innerHTML = htmlString;
+  return container.firstElementChild;
+}
+
+function onCardClick(card) {
+  card.classList.toggle("flipped");
+}
+
+window.addEventListener("load", function () {
+  boardArr = boardArr.map((_el) => {
+    return {
+      src: "https://api.dicebear.com/9.x/open-peeps/svg",
+      alt: "avatar",
+    };
+  });
+  boardArr.forEach((el) => {
+    gameboard.appendChild(
+      createElementFromHTML(createCardHTML(el.src, el.alt))
+    );
+  });
+});
